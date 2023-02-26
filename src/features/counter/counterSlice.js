6ba70a08fb08a6caebef32c6f71610453dc2e7ff
ShakurUrRahman <you@example.comshakururrahman@gmail.com>
2023-02-26 13:20:51 +0600

    createAsyncThunk implemented for data fetching

--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -1,8 +1,19 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    count: 0
+    count: 0,
+    products: [],
+    isLoading: false,
+    isError: false,
+    error: "",
 };
+
+export const getProducts = createAsyncThunk = ("action/name", async () => {
+    const res = await fetch('http://localhost:5000/products')
+    const data = await res.json();
+
+    return data.data
+})
 
 const counterSlice = createSlice({
     name: "counter",
@@ -14,7 +25,24 @@ const counterSlice = createSlice({
         decrement: (state, action) => {
             state.count -= action.payload;
         }
-    }
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(getProducts.pending, (state, action) => {
+                state.isLoading = true;
+                state.isError = false;
+            })
+            .addCase(getProducts.fulfilled, (state, action) => {
+                state.products = action.payload;
+                state.isLoading = false;
+            })
+            .addCase(getProducts.rejected, (state, action) => {
+                state.products = [];
+                state.isLoading = false;
+                state.isError = true;
+                state.error = action.error.message;
+            })
+    },
 });
 
 export const { increment, decrement } = counterSlice.actions;
